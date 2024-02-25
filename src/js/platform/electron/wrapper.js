5ad7883e836c70c303594c0b8a94de1de39567c6
Danyil Hryhoriev <dengr1065@gmail.com>
2024-02-25 17:08:24 +0200

    Remove Steam overlay "fix"
    As the community edition is not published on Steam, it's not needed.
    Aside from that, things may have changed and it may work flawlessly with
    newer Electron versions.

--- src/js/platform/electron/wrapper.js
+++ src/js/platform/electron/wrapper.js
@@ -13,26 +13,12 @@ export class PlatformWrapperImplElectron extends PlatformWrapperImplBrowser {
             puzzle: false,
         };
 
-        this.steamOverlayCanvasFix = document.createElement("canvas");
-        this.steamOverlayCanvasFix.width = 1;
-        this.steamOverlayCanvasFix.height = 1;
-        this.steamOverlayCanvasFix.id = "steamOverlayCanvasFix";
-
-        this.steamOverlayContextFix = this.steamOverlayCanvasFix.getContext("2d");
-        document.documentElement.appendChild(this.steamOverlayCanvasFix);
-
-        this.app.ticker.frameEmitted.add(this.steamOverlayFixRedrawCanvas, this);
-
         this.app.storage = new StorageImplElectron(this);
         this.app.achievementProvider = new SteamAchievementProvider(this.app);
 
         return this.initializeAchievementProvider()
             .then(() => this.initializeDlcStatus())
             .then(() => PlatformWrapperInterface.prototype.initialize.call(this));
-    }
-
-    steamOverlayFixRedrawCanvas() {
-        this.steamOverlayContextFix.clearRect(0, 0, 1, 1);
     }
 
     getId() {
